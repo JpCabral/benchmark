1b72ccac5f6ab724494ec995ba6e5047b15b9255
João Paulo <jpocabral@gmail.com>
2019-04-30 13:32:58 -0300

    Reparo na criaçao de arquivos

--- sniffer.py
+++ sniffer.py
@@ -4,5 +4,8 @@ import os
 def captura_pcap(arquivo, interface, janeladetempo):
     pwd = (subprocess.getoutput('pwd')) + '/'
     fullarquivo = pwd + arquivo
-    os.system("touch " + arquivo)
-    os.system("dumpcap -i " + interface + " -a duration:" + str(janeladetempo) + " -w " + fullarquivo)
+
+    try:
+        os.system("dumpcap -i " + interface + " -a duration:" + str(janeladetempo) + " -w " + fullarquivo)
+    except FileNotFoundError:
+        os.system("touch " + arquivo)
