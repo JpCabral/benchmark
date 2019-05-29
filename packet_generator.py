e6738c81c9278c557307399a5c9a084e13512f35
João Paulo Oliveira Cabral <jpocabral@gmail.com>
2019-05-29 16:47:16 -0300

    Remoção de código desnecessário

--- packet_generator.py
+++ packet_generator.py
@@ -4,5 +4,3 @@ import subprocess
 def generate_packets(arquivo_amostra, interface, loops):
     arquivo_amostra = subprocess.getoutput('pwd') + '/pcap_samples/' + arquivo_amostra
     os.system("tcpreplay -i "+interface+" -K --loop="+loops+" -x 0.67 "+arquivo_amostra+" &")
-
-generate_packets(arquivo_amostra="15min_fila_wifi.pcapng", interface="wlan0", loops="19")
